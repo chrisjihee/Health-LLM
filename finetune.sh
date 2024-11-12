35972854147e0b849eefcbfb5b32dd35ee04715c
Jihee Ryu <chrisjihee@etri.re.kr>
2024-11-13 00:15:07 +0900

    Update minor for one-line command

--- finetune.sh
+++ finetune.sh
@@ -18,3 +18,4 @@ torchrun --nproc_per_node=8 --master_port=2023 medalpaca/train.py \
     --global_batch_size 128 \
     --per_device_batch_size 4 \
     --num_epochs 5
+# --model "medalpaca/medalpaca-7b" --data_path "data/generated_data_json/PMData_readiness_train_all.json" --output_dir "output/medalpaca-7b-tuned" --train_in_8bit False --use_lora False --bf16 True --tf32 True --fp16 False --gradient_checkpointing True --global_batch_size 128 --per_device_batch_size 4 --num_epochs 5
